14d7904e897b407e75c46beb400be05bc2ab67c0
robinsonruth109 <robinsonruth109@gmail.com>
2026-04-20 19:53:43 +0600

    fix prisma mariadb adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,13 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
-import mysql from "mysql2/promise";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function prismaClientSingleton() {
-  const pool = mysql.createPool({
-    uri: process.env.DATABASE_URL,
-  });
-
-  const adapter = new PrismaMariaDb(pool);
+  const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
   return new PrismaClient({
     adapter,
